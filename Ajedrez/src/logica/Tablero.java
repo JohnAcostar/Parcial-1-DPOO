3b5d300151fefff7cb2d922a59e516ad987f06e7
John_Acosta <ja.acostar1@uniandes.edu.co>
2026-02-18 10:14:08 -0500

    Add Tablero class for chess board logic

---
+++ Ajedrez/src/logica/Tablero.java
@@ -0,0 +1,11 @@
+package logica;
+
+public class Tablero {
+
+    public static final int TAMANIO = 8;
+
+    public boolean posicionValida(int fila, int columna) {
+        return fila >= 1 && fila <= TAMANIO &&
+               columna >= 1 && columna <= TAMANIO;
+    }
+}
